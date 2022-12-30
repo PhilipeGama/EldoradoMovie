a5cd1b47cb84fd6812d2154b18fd0697b1b26dbf
Philipe Ferreira <philfer@motorola.com>
2022-12-29 20:29:33 -0400

    initial implementation pagination

--- frontend/src/app/components/pagination/pagination.component.ts
+++ frontend/src/app/components/pagination/pagination.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginationComponent implements OnInit {
 
+  pageNumber = [1,2,3];
+
+  meta = {
+    totPage: 10,
+    currentPage: 1,
+    nextPage: 10,
+    itemsPerPage: 10,
+    totItems: 50,
+    lastPage: 5,
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  nextPage(){
+    console.log(this.pageNumber[2])
+    if(this.meta.totPage > this.pageNumber[2]) {
+      const newArray =  this.pageNumber.map((v) => v + 1);
+      this.pageNumber = newArray;
+    }
+
+  }
+
+  previousPage(){
+    if(this.pageNumber[0] > 1){
+      const newArray =  this.pageNumber.map((v) => v - 1);
+      this.pageNumber = newArray;
+    }
+
+  }
+
+  currentPage(page: number){
+    this.meta.currentPage = page;
+    console.log(page)
+  }
+
 
 }
